ee06efe06eb42496ee07f0a866a2aa49fac74d6a
Milan <milan903575@gmail.com>
2025-12-08 20:00:29 +0530

    connnection.php credentials

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 $conn = mysqli_connect(
-    "sql100.infinityfree.com", // MySQL Host Name
-    "if0_38023308",            // MySQL User Name
-    "milan903575",             // MySQL Password
-    "if0_38023308_ehr"         // MySQL DB Name
+    "", // MySQL Host Name
+    "", // MySQL User Name
+    "", // MySQL Password
+    ""  // MySQL DB Name
 );
 
 // Check connection
